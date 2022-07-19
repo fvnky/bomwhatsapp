927bd37c91c94b6eb7ffe1b95d4c885d4b4e5584
fvnky <109422439+fvnky@users.noreply.github.com>
2022-07-19 15:34:55 +0700

    Update callwhatsapp.php

--- callwhatsapp.php
+++ callwhatsapp.php
@@ -12,7 +12,7 @@ function send($phone){
                 echo $asw."\n";
 }
 echo "COPYRIGHT ; SGBTEAM\n\n";
-echo "Nomor\nInput : ";
+echo "kontol : ";
 $nomor = trim(fgets(STDIN));
 $execute = send($nomor);
 print $execute;
